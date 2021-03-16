4623969b2938b03c8ad0e2ad4f39156f94415c07
jrmcgarvey <jmcgarve@bellsouth.net>
2021-03-16 18:41:09 -0400

    this is my lesson

---
+++ years_to_hours.rb
@@ -0,0 +1,5 @@
+puts "Enter a number of years"
+years = gets.chomp # this returns a string
+years = years.to_i # this converts a string to an integer
+hours = years * 365 * 24
+puts "That's #{hours} hours."  
