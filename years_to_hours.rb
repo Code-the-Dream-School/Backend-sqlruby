b91dda36d34c3e4c1deaa0309a809d4708c6eef5
rojina <125151480+rojina77@users.noreply.github.com>
2023-10-16 20:37:30 -0700

    Update years_to_hours.rb

--- years_to_hours.rb
+++ years_to_hours.rb
@@ -19,27 +19,24 @@
     #decade is ten years
     # 365 days in a year
 
-def years_to_hours
+
     puts "Enter a number of years"
     years = gets.chomp # this returns a string
     years = years.to_i # this converts a string to an integer
     hours = years * 365 * 24
     puts "That's #{hours} hours"   
     returns hours
-end
 
-def decades_to_minutes
+
     puts "Enter number of decades"
     decades = gets.chomp.to_i
     minutes = decades *10 * 365 *24 * 60
     puts "That's #{minutes} minutes."
     returns minutes
-end
 
-def age_to_seconds
+
     puts "enter your age"
     age = gets.chomp.to_i
     seconds = 60 * 60 * age * 365 * 24 
     puts "that's #{seconds} seconds"
     returns seconds
-end
